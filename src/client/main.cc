b7e22a375d9c561fe8b9990ed8f24f8af0ccec4b
Josh Holtrop <jholtrop@gmail.com>
2012-09-06 21:49:14 -0400

    add command-line options for antialias_level and compatibility_context

--- src/client/main.cc
+++ src/client/main.cc
@@ -1,17 +1,33 @@
 
+#include <stdlib.h>
 #include <getopt.h>
 #include "Client.h"
 
 int main(int argc, char *argv[])
 {
     bool fullscreen = false;
+    unsigned int antialias_level = 0u;
+    bool compatibility_context = false;
+
+    struct option longopts[] = {
+        {"antialias-level", required_argument, NULL, 'a'},
+        {"compatibility", no_argument, NULL, 'c'},
+        {"fullscreen", no_argument, NULL, 'f'},
+        {NULL, 0, NULL, 0}
+    };
     for (;;)
     {
-        int c = getopt_long(argc, argv, "f", NULL, NULL);
+        int c = getopt_long(argc, argv, "a:cf", longopts, NULL);
         if (c == -1)
             break;
         switch (c)
         {
+            case 'a':
+                antialias_level = atoi(optarg);
+                break;
+            case 'c':
+                compatibility_context = true;
+                break;
             case 'f':
                 fullscreen = true;
                 break;
