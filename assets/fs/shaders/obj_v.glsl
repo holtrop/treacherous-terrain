4a6d38ddb96cb35200952873661d89bd14b1a224
Josh Holtrop <jholtrop@gmail.com>
2012-09-10 21:29:11 -0400

    add projection and modelview matrices to obj shader

--- assets/fs/shaders/obj_v.glsl
+++ assets/fs/shaders/obj_v.glsl
@@ -1,5 +1,7 @@
 
 uniform float scale;
+uniform mat4 projection;
+uniform mat4 modelview;
 
 attribute vec3 pos;
 attribute vec3 normal;
@@ -9,7 +11,7 @@ varying vec3 normal_i;
 
 void main(void)
 {
-    gl_Position = gl_ModelViewProjectionMatrix * vec4(scale * pos, 1);
+    gl_Position = projection * modelview * vec4(scale * pos, 1);
     pos_i = gl_Position.xyz;
-    normal_i = gl_NormalMatrix * normal;
+    normal_i = modelview * vec4(normal, 1);
 }
